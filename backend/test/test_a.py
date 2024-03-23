f86f74946d37a9c5227ac8df6875971248d7db5d
eri24816 <a931eric@gmail.com>
2024-03-23 22:34:22 +0800

    test: more tests

--- backend/test/test_a.py
+++ backend/test/test_a.py
@@ -1,11 +1,14 @@
 
 from grapycal.core.workspace import Workspace
 from grapycal.stores import main_store
+import pytest
 
-def test_node():
+def setup_workspace():
     workspace = Workspace(port=8766, host="localhost", path="workspace.grapycal", workspace_id=0)
-    workspace.run(False)
-    
+    workspace.run(run_runner=False)
+
+def test_edge_creation():
+    setup_workspace()
     from grapycal_builtin.interaction.execNode import ExecNode
     n1 = main_store.main_editor.create_node(ExecNode)
     n2 = main_store.main_editor.create_node(ExecNode)
@@ -16,4 +19,30 @@ def test_node():
     assert e.tail.get() == p1
     assert e.head.get() == p2
     
-   
+def test_max_edges():
+    setup_workspace()
+    from grapycal_builtin.interaction.execNode import ExecNode
+    n1 = main_store.main_editor.create_node(ExecNode)
+    n2 = main_store.main_editor.create_node(ExecNode)
+    p1 = n1.add_out_port('out', max_edges=2)
+    p2 = n2.add_in_port('in')
+
+    e1 = main_store.main_editor.create_edge(p1, p2)
+    e2 = main_store.main_editor.create_edge(p1, p2)
+
+    with pytest.raises(Exception):
+        e3 = main_store.main_editor.create_edge(p1, p2)
+
+def test_edge_deletion():
+    setup_workspace()
+    from grapycal_builtin.interaction.execNode import ExecNode
+    n1 = main_store.main_editor.create_node(ExecNode)
+    n2 = main_store.main_editor.create_node(ExecNode)
+    p1 = n1.add_out_port('out')
+    p2 = n2.add_in_port('in')
+    e = main_store.main_editor.create_edge(p1, p2)
+
+    main_store.main_editor._delete([e.get_id()])
+
+    assert p1.edges == []
+    assert p2.edges == []
