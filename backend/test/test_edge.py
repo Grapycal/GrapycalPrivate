e8352bfe94ee48ba9d02e6e9eb7077fff3cdcdd9
eri24816 <a931eric@gmail.com>
2024-03-24 21:16:31 +0800

    use Test1Node for testing

--- backend/test/test_edge.py
+++ backend/test/test_edge.py
@@ -2,8 +2,8 @@ import pytest
 from utils import setup_workspace, main_editor
 
 def test_edge_creation(setup_workspace,main_editor):
-    n1 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n2 = main_editor.create_node('grapycal_builtin.ExecNode')
+    n1 = main_editor.create_node('grapycal_test.Test1Node')
+    n2 = main_editor.create_node('grapycal_test.Test1Node')
     p1 = n1.add_out_port('out')
     p2 = n2.add_in_port('in')
     e = main_editor.create_edge(p1, p2)
@@ -15,8 +15,8 @@ def test_edge_creation(setup_workspace,main_editor):
     assert set(main_editor.get_children()) == {n1, n2, e}
     
 def test_max_edges(setup_workspace,main_editor):
-    n1 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n2 = main_editor.create_node('grapycal_builtin.ExecNode')
+    n1 = main_editor.create_node('grapycal_test.Test1Node')
+    n2 = main_editor.create_node('grapycal_test.Test1Node')
     p1 = n1.add_out_port('out', max_edges=2)
     p2 = n2.add_in_port('in')
 
@@ -27,8 +27,8 @@ def test_max_edges(setup_workspace,main_editor):
         e3 = main_editor.create_edge(p1, p2)
 
 def test_edge_deletion(setup_workspace,main_editor):
-    n1 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n2 = main_editor.create_node('grapycal_builtin.ExecNode')
+    n1 = main_editor.create_node('grapycal_test.Test1Node')
+    n2 = main_editor.create_node('grapycal_test.Test1Node')
     p1 = n1.add_out_port('out')
     p2 = n2.add_in_port('in')
     e = main_editor.create_edge(p1, p2)
@@ -39,8 +39,8 @@ def test_edge_deletion(setup_workspace,main_editor):
     assert p2.edges == []
 
 def test_edge_deletion_on_node_deletion(setup_workspace,main_editor):
-    n1 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n2 = main_editor.create_node('grapycal_builtin.ExecNode')
+    n1 = main_editor.create_node('grapycal_test.Test1Node')
+    n2 = main_editor.create_node('grapycal_test.Test1Node')
     p1 = n1.add_out_port('out')
     p2 = n2.add_in_port('in')
     e = main_editor.create_edge(p1, p2)
@@ -57,15 +57,21 @@ def test_edge_move(setup_workspace,main_editor):
     An edge's head or tail can be moved to another port dynamically.
     It happens when the user drags the edge to another port.
     '''
-    n1 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n2 = main_editor.create_node('grapycal_builtin.ExecNode')
-    n3 = main_editor.create_node('grapycal_builtin.ExecNode')
+    n1 = main_editor.create_node('grapycal_test.Test1Node')
+    n2 = main_editor.create_node('grapycal_test.Test1Node')
+    n3 = main_editor.create_node('grapycal_test.Test1Node')
     p1 = n1.add_out_port('out')
     p2 = n2.add_in_port('in')
     p3 = n3.add_in_port('in')
+
+    # Create an edge between p1 and p2
     e = main_editor.create_edge(p1, p2)
 
-    # Move the edge to another port
+    assert p1.edges == [e]
+    assert p2.edges == [e]
+    assert p3.edges == []
+
+    # Move the edge's head from p2 to p3
     e.head.set(p3)
 
     assert p1.edges == [e]
