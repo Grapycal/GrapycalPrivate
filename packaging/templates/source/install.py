cd623a12b7a0fc62b4d28a4db5b0a6a410f4e968
eri24816 <a931eric@gmail.com>
2024-04-19 14:49:00 +0800

    fix: check pip version when install

--- packaging/templates/source/install.py
+++ packaging/templates/source/install.py
@@ -12,8 +12,14 @@ os.chdir(here)
 print('Checking python 3.11 is installed...')
 try:
     assert sys.version_info >= (3, 11)
+    assert sys.version_info < (3, 12)
 except AssertionError:
     print('Python 3.11 is required. Please install it.')
+    sys.exit(1)
+
+# check if pip is installed
+if os.system('pip --version') != 0:
+    print('pip is not installed. Please install it.')
     sys.exit(1)
     
 # 2. Install packages
