580b16eede9b389d4b2d8c7810aa38657e758761
eri24816 <a931eric@gmail.com>
2024-04-22 17:59:10 +0800

    chore: use laucher.py in dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python scripts/build_frontend.py &&\
-python entry/run.py --backend-path "backend/src" --frontend-path "frontend/dist" --extensions-path "extensions" --port 7943 --cwd "dev_cwd"
+python entry/launcher.py --backend-path "backend/src" --frontend-path "frontend/dist" --extensions-path "extensions" --port 7943 --cwd "dev_cwd"
