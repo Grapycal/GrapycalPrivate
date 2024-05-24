91c250d8dc4a944107386f7ffcf49e888103448e
eri24816 <a931eric@gmail.com>
2024-05-24 21:51:42 +0800

    chore: fix new_minor.py

--- scripts/new_minor.py
+++ scripts/new_minor.py
@@ -38,12 +38,10 @@ def new_minor():
     # this trick avoids merge conflicts when merging the release branch back into main, if needed
 
     cmd(f"git branch {release_branch_name}")
+    bump_and_commit(current_version, new_main_branch_version)
+
     cmd(f"git checkout {release_branch_name}")
     bump_and_commit(current_version, release_version)
-    cmd(f"git tag {release_tag}")
-
-    cmd("git checkout main")
-    bump_and_commit(current_version, new_main_branch_version)
 
 
 if __name__ == "__main__":
