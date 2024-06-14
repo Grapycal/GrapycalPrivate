c0ceb48cb30276562d56821700aa11b62e64b34f
GreenSlime1024 <greenslime1024@gmail.com>
2024-06-14 19:25:32 +0800

    fix: discord.app_commands.errors.CommandAlreadyRegistered error

--- extensions/grapycal_discordpy/__init__.py
+++ extensions/grapycal_discordpy/__init__.py
@@ -81,6 +81,11 @@ class DiscordCommandNode(Node):
         params_str = ', '.join([f'{key}:{value}' for key, value in params.items()])
         params_str_without_type = ', '.join(params.keys())
         
+        try:
+            bot.tree.remove_command(cmd_name)
+        except:
+            pass
+
         exec(f'@bot.tree.command(name="{cmd_name}", description="{cmd_description}")\n'
              f'async def {cmd_name}(interaction:Interaction, {params_str}):\n'
              f'    cb.push("callback", (interaction, {params_str_without_type}))\n'
