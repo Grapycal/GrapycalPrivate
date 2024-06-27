6beef2b58b8d3612c773699072559765cd2f3ec7
eri24816 <a931eric@gmail.com>
2024-06-27 23:59:32 +0800

    bump: 0.18.3 -> 0.18.4

--- electron/main.js
+++ electron/main.js
@@ -126,7 +126,7 @@ const createWindow = (port) => {
 	syncInterpreterListWithMenu()
 	app.setAboutPanelOptions({
 		applicationName: 'Grapycal', 
-		applicationVersion: '0.18.3',
+		applicationVersion: '0.18.4',
 		copyright: 'Grapycal Team',
 		iconPath: path.join(__dirname, 'frontend', 'dist', 'icon.png').replace("app.asar", "app.asar.unpacked")
 	})
